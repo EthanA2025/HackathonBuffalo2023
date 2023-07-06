1db55804059b71a21e0fcf99462d60d5cce96848
etan Memeignton <eja9979@g.rit.edu>
2023-07-06 16:50:33 -0400

    regression model for data

--- gengrades.py
+++ gengrades.py
@@ -12,7 +12,7 @@ data = []
 # Generate the data for each student
 for i in range(num_students):
     # Generate GPA values
-    gpa_values = np.clip(np.random.normal(gpa_mean, gpa_std, num_years), 0, 4)
+    gpa_values = np.round(np.clip(np.random.normal(gpa_mean, gpa_std, num_years), 0, 4), decimals=2)
     
     # Compute the average GPA
     avg_gpa = np.mean(gpa_values)
@@ -21,11 +21,11 @@ for i in range(num_students):
     dropout = 1 if avg_gpa <= 1.5 else 0
     
     # Append the student's data to the list
-    data.append([i+1] + list(gpa_values) + [dropout])
+    data.append([i+1] + list(list(gpa_values)) + [dropout])
 
 # Define the column headers
-headers = ['student_id'] + [f'gpa_year_{i+1}' for i in range(num_years)] + ['dropout']
-
+headers = ['student_id'] + ['time'] + ['dropout']
+#f'gpa_year_{i+1}' for i in range(num_years)
 # Write the data to a CSV file
 with open('student_data.csv', 'w', newline='') as f:
     writer = csv.writer(f)
---
+++ regression_model.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler
+
+# load your dataset
+# let's assume the data has two columns: "GPA" and "dropout" (1 if the student dropped out, 0 otherwise)
+df = pd.read_csv('student_data.csv')
+
+# split the dataset into features (X) and target (y)
+X = df[['GPA']]
+y = df['dropout']
+
+# it's a good practice to scale the features so they have a mean of 0 and standard deviation of 1
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# create the logistic regression model
+model = LogisticRegression()
+
+# train the model
+model.fit(X_train, y_train)
+
+# test the model
+y_pred = model.predict(X_test)
+
+# print the classification report
+print(classification_report(y_test, y_pred))
