e94683606bf0ae3f184479857fe48bc876c60f92
etan Memeignton <eja9979@g.rit.edu>
2023-07-07 12:00:03 -0400

    sadas

--- buffalo-prep-hackathon/src/App.js
+++ buffalo-prep-hackathon/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainComp from './MainComponent';
 import CSVLoad from './CSVLoad';
+import { Container } from 'reactstrap';
 
 function App() {
   return (
     <div>
       <MainComp />
       <CSVLoad />
-      <img src="data.jpg" alt="regression data"></img>
+      <Container>
+        <img src="/regression.png" alt="data"></img>
+      </Container>
     </div>
   );
 }
